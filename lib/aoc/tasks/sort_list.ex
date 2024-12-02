e88276b829f86dc75ff52922ef12da5c995f0df1
Matthew Sells <matthew.sells@proton.me>
2024-12-01 20:19:53 -0600

    refactor day 1

--- lib/aoc/tasks/sort_list.ex
+++ lib/aoc/tasks/sort_list.ex
@@ -1,29 +1,29 @@
 defmodule Aoc.Tasks.SortList do
+  # TODO: Clean up and add docs
   def difference(input) do
     input
-    |> String.split("\n", trim: true)
-    |> Enum.reduce({[], []}, fn line, {list1, list2} ->
-      [a, b] = String.split(line, " ", trim: true) |> Enum.map(&String.to_integer/1)
-      {[a | list1], [b | list2]}
-    end)
+    |> to_split_lists()
     |> (&{Enum.sort(elem(&1, 0)), Enum.sort(elem(&1, 1))}).()
     |> (&Enum.zip(elem(&1, 0), elem(&1, 1))).()
     |> Enum.reduce(0, fn {a, b}, acc -> acc + abs(a - b) end)
   end
 
   def similarity(input) do
-    {source, comp} =
-      input
-      |> String.split("\n", trim: true)
-      |> Enum.reduce({[], []}, fn line, {list1, list2} ->
-        [a, b] = String.split(line, " ", trim: true) |> Enum.map(&String.to_integer/1)
-        {[a | list1], [b | list2]}
-      end)
+    {source, comp} = to_split_lists(input)
 
     Enum.map(source, fn n ->
       x = Enum.count(comp, &(&1 == n))
       n * x
     end)
     |> Enum.sum()
+  end
+
+  defp to_split_lists(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.reduce({[], []}, fn line, {list1, list2} ->
+      [a, b] = String.split(line, " ", trim: true) |> Enum.map(&String.to_integer/1)
+      {[a | list1], [b | list2]}
+    end)
   end
 end
