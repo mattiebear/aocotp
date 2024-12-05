c32c48a02a452268190c50e1790edd09b5bfd72b
Matthew Sells <matthew.sells@proton.me>
2024-12-05 09:27:08 -0600

    complete day 3

--- lib/aoc/tasks/multiplier.ex
+++ lib/aoc/tasks/multiplier.ex
@@ -2,7 +2,7 @@ defmodule Aoc.Tasks.Multiplier do
   @behaviour Aoc.Task
 
   def runners do
-    {&multiply/1, &multiply/1}
+    {&multiply/1, &conditional_multiply/1}
   end
 
   @spec multiply(binary()) :: integer()
@@ -12,7 +12,31 @@ defmodule Aoc.Tasks.Multiplier do
     |> Enum.sum()
   end
 
+  def conditional_multiply(input) do
+    input = String.replace(input, "\n", "")
+
+    break_commands(input)
+    |> Enum.filter(&String.match?(&1, ~r/^(?!don't).+/))
+    |> Enum.join()
+    |> multiply()
+  end
+
   defp multiply_captures([_cap | [a, b]]) do
     String.to_integer(a) * String.to_integer(b)
+  end
+
+  defp break_commands(input) do
+    do_break_commands(input, [])
+  end
+
+  defp do_break_commands(text, list) do
+    case Regex.run(~r/.don't\(\)|.do\(\)/, text, return: :index) do
+      [{index, _}] ->
+        {head, tail} = String.split_at(text, index + 1)
+        do_break_commands(tail, [head | list])
+
+      _ ->
+        [text | list]
+    end
   end
 end
--- test/tasks/multiplier_test.exs
+++ test/tasks/multiplier_test.exs
@@ -1,10 +1,14 @@
 defmodule Aoc.Tasks.MultiplierTest do
   use ExUnit.Case
 
-  @test_input "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+  test "multiplier/1 finds all of the multiplier calls" do
+    input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    assert Aoc.Tasks.Multiplier.multiply(input) == 161
+  end
 
   @tag :filter
-  test "multiplier/1 finds all of the multiplier calls" do
-    assert Aoc.Tasks.Multiplier.multiply(@test_input) == 161
+  test "conditional_multiply/1 finds all of the multiplier calls" do
+    input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    assert Aoc.Tasks.Multiplier.conditional_multiply(input) == 48
   end
 end
